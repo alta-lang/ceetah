b7fd755e7c2acbc5fcfb58ab2daff2baa585e5c7
Ariel Abreu <facekapow@outlook.com>
2019-05-22 08:37:14 -0400

    Make sure to add parenthesis around binary operations
    Otherwise, expressions could be interpretted differently

--- src/ast/binary-operation.cpp
+++ src/ast/binary-operation.cpp
@@ -5,7 +5,7 @@ const Ceetah::AST::NodeType Ceetah::AST::BinaryOperation::nodeType() {
 };
 
 std::string Ceetah::AST::BinaryOperation::toString() {
-  return '(' + left->toString() + ") " + OperatorType_operators[(uint8_t)type] + " (" + right->toString() + ')';
+  return "((" + left->toString() + ") " + OperatorType_operators[(uint8_t)type] + " (" + right->toString() + "))";
 };
 
 bool Ceetah::AST::BinaryOperation::operator ==(const Ceetah::AST::BinaryOperation& other) {
